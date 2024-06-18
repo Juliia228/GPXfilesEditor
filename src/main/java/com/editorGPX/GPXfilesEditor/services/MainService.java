6b01508c86aaee5ffb9e7bd123190d92ebf87873
Juliia228 <miss-komarova2017@yandex.ru>
2024-06-18 13:16:53 +0300

    Add main service

---
+++ src/main/java/com/editorGPX/GPXfilesEditor/services/MainService.java
@@ -0,0 +1,34 @@
+package com.editorGPX.GPXfilesEditor.services;
+
+import com.editorGPX.GPXfilesEditor.models.Session;
+import io.jenetics.jpx.GPX;
+import lombok.Data;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+
+@Service
+@Data
+public class MainService {
+    private Session lastSession = new Session();
+
+    public MainService() throws IOException {
+    }
+
+    public String getFullPathToFile() {
+        return lastSession.getFullPathToFile();
+    }
+
+    public GPX getGPX() {
+        return lastSession.getGpx();
+    }
+
+    public void setFilename(String name) {
+        lastSession.setFileName(name);
+        lastSession.setFullPathToFile();
+    }
+
+    public void setGPX(GPX gpx) throws IOException {
+        lastSession.setGpx(gpx);
+    }
+
+}
